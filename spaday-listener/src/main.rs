5f1b322635ac1343967bd02d9f1f5422450f0082
technetos <mrgould93@gmail.com>
2025-02-20 22:02:24 -0500

    Break out decoder into its own thread, only try and decode packets with a payload starting with KNOCKKNOCK, pull in pgp crate, setup inital pgp stuff..more to come

--- spaday-listener/src/main.rs
+++ spaday-listener/src/main.rs
@@ -1,4 +1,9 @@
+use std::fs;
+use std::path::PathBuf;
+use std::sync::mpsc::{channel, Sender};
+
 use clap::Parser;
+use pgp::{Deserializable, SignedPublicKey};
 use pnet::datalink::{self, Channel};
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
 use pnet::packet::ip::IpNextHeaderProtocols;
@@ -6,17 +11,41 @@ use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::udp::UdpPacket;
 use pnet::packet::Packet;
 
+const KNOCKKNOCK: &[u8] = b"KNOCKKNOCK";
+
 #[derive(Parser, Debug)]
 #[command(version)]
 struct Args {
     #[arg(short)]
     interface: String,
+
+    #[arg(short)]
+    pub_key_file: PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
     let args = Args::parse();
+
+    let (public_key, _) = SignedPublicKey::from_string(&fs::read_to_string(args.pub_key_file)?)?;
+
+    let (tx, rx) = channel::<Vec<u8>>();
+
+    let decoder_thread = std::thread::spawn(move || loop {
+        match rx.recv() {
+            Ok(bytes) => {
+                if let Err(e) = decode_payload(&bytes) {
+                    tracing::error!("An error occurred when decrypting the udp payload: {e}");
+                }
+            }
+            Err(e) => {
+                tracing::error!(
+                    "An error occurred when reading from the decoder thread channel: {e}"
+                );
+            }
+        }
+    });
 
     let interfaces = pnet::datalink::interfaces();
     let interface = interfaces
@@ -30,7 +59,7 @@ fn main() -> anyhow::Result<()> {
             match rx.next() {
                 Ok(packet) => {
                     if let Some(packet) = EthernetPacket::new(packet) {
-                        handle_packet(&packet)?;
+                        handle_packet(&packet, &tx)?;
                     }
                 }
                 Err(e) => {
@@ -39,20 +68,37 @@ fn main() -> anyhow::Result<()> {
             }
         }
     }
+
+    decoder_thread
+        .join()
+        .expect("Failed to join decoder thread");
+
     Ok(())
 }
 
-fn handle_packet(ethernet: &EthernetPacket) -> anyhow::Result<()> {
+fn handle_packet(ethernet: &EthernetPacket, tx: &Sender<Vec<u8>>) -> anyhow::Result<()> {
     if let EtherTypes::Ipv4 = ethernet.get_ethertype() {
         if let Some(header) = Ipv4Packet::new(ethernet.payload()) {
             if let IpNextHeaderProtocols::Udp = header.get_next_level_protocol() {
                 if let Some(udp) = UdpPacket::new(header.payload()) {
                     let udp_payload = udp.payload();
-                    dbg!(udp_payload);
+                    if udp_payload.starts_with(KNOCKKNOCK) {
+                        let bytes = udp_payload[KNOCKKNOCK.len()..].to_vec();
+                        if let Err(e) = tx.send(bytes) {
+                            tracing::error!(
+                                "An error occurred when sending to the decoder thread: {e}"
+                            );
+                        }
+                    }
                 }
             }
         }
     }
 
+    Ok(())
+}
+
+fn decode_payload(bytes: &[u8]) -> anyhow::Result<()> {
+    dbg!(bytes);
     Ok(())
 }
